b74c78a12012d006bcbc1812aac8f16210c17c39
theussmx <159255740+theussmx@users.noreply.github.com>
2025-04-29 15:46:30 -0300

    Add files via upload

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,6 @@
 import Header from '../../components/Header'
 import Carousel from '../../components/Carousel'
 import style from './Home.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
@@ -10,33 +8,28 @@ function Home() {
       <Header transp={true} />
       <section className={style.homeSection}>
         <h1>SEJA BEM-VINDO</h1>
-        <img src="/logo-aberta.png" alt="Ler Mais" />
-        <p>No nosso site, você lê, interage com autores e ainda ganha recompensas por cada interação!</p>
-        <a href="#exploreSection">
-          <FontAwesomeIcon icon={faSearch} /> Explorar
-        </a>
+        <img src="/logo-aberta.png" alt="" />
       </section>
-      <section id="exploreSection">
-        <Carousel />
-        <div className={style.autorsContainer}>
-          <h2>Autores populares</h2>
-          <div className={style.itemCardContainer}>
-            <div className={style.itemCard}>
-              <img src="https://placehold.co/200x150" alt="Placeholder 1" />
-              <p>Autor 1</p>
-            </div>
-            <div className={style.itemCard}>
-              <img src="https://placehold.co/200x150" alt="Placeholder 2" />
-              <p>Autor 2</p>
-            </div>
-            <div className={style.itemCard}>
-              <img src="https://placehold.co/200x150" alt="Placeholder 3" />
-              <p>Autor 3</p>
-            </div>
+      <Carousel />
+      <div className={style.autorsContainer}>
+        <h2>Autores populares</h2>
+        <div className={style.itemCardContainer}>
+          <div className={style.itemCard}>
+            <img src="https://placehold.co/200x150" alt="Placeholder 1" />
+            <p>Autor 1</p>
+          </div>
+          <div className={style.itemCard}>
+            <img src="https://placehold.co/200x150" alt="Placeholder 2" />
+            <p>Autor 2</p>
+          </div>
+          <div className={style.itemCard}>
+            <img src="https://placehold.co/200x150" alt="Placeholder 3" />
+            <p>Autor 3</p>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 }
+
 export default Home
